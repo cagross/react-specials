b423a60f9df38c3779865bb70aa30883b11f64c4
Carl Gross <10165488+cagross@users.noreply.github.com>
2019-06-30 16:23:46 +0700

    Update thumbnails.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 // import React, { Component } from 'react';//This needs to be present in order to use React class components.  As of 3/5/19, I probably do not need it, since I plan to re-factor everything to use React hooks and functional components, instead of classes.
 import React from 'react';
 // import logo from './logo.svg';
+import img_meat from './images/all-meat-250.jpg';
 import img_beef from './images/beef-250.jpg';
 import img_chicken from './images/chicken-250.jpg';
 import img_ribs from './images/ribs-250.jpg';
+
+
 
 import './App.css';
 import { useState } from 'react';// This needs to be present in order to use the 'useState' hook.
@@ -61,21 +64,40 @@ function OutSpec(props) {
 		];
 
 		// if (Object.entries(sizes).length !== 0 && sizes.constructor === Object) {// Check if the sizes object is empty or not.  If it is not empty, execute code.
-		if (typeof sizes.items == 'object') {// Check if  sizes.items has been set.  If so, execute code.
+		// if (typeof sizes.items == 'object') {// Check if  sizes.items has been set.  If so, execute code.
+		if (typeof sizes == 'object') {// Check if  sizes.items has been set.  If so, execute code.
 
-			console.log(sizes.items);
+			// var myObject = sizes.items;
+			var myObject = sizes;
 
-			var myObject = sizes.items;
-			
+			console.log(myObject);
+
+/* Begin code to create a new object containing only 'meat' and 'deli' items. */
+			const filteredObjects = Object.keys(myObject).filter(key => {
+				if ((myObject[key]["category_names"][0] === "Meat") ||
+					myObject[key]["category_names"][0] === "Deli") {
+					return true;
+				}
+			});
+			var test = filteredObjects.map(myFunction);
+			function myFunction(item) {
+				return myObject[item];
+			}
+			// console.log(test);
+/* End code to create a new object containing only 'meat' and 'deli' items. */
 
 			return Object.keys(myObject).map(function (key, index) {
-				if (sizes.items[key]["category_names"][0] === "Meat" || 
-					sizes.items[key]["category_names"][0] === "Deli") {
+				if (myObject[key]["category_names"][0] === "Meat" || 
+					myObject[key]["category_names"][0] === "Deli") {
 					
-						var str = sizes.items[key]["display_name"].toLowerCase();
+						var str = myObject[key]["display_name"].toLowerCase();
 						
 						var pos;
-						var i;
+						var i
+						// var j = 0;
+						j++;
+						// console.log(j);
+
 						
 						if (props.meat === "Poultry") {
 							// console.log(str);
@@ -111,14 +133,32 @@ function OutSpec(props) {
 						// var pos = str.search(props.meat);
 
 						if (pos >= 0) {
-							j++;
+							// j++;
 							// console.log(j);
+							// <ReactTransitionGroup.TransitionGroup>
+
 
 							return (
+								
+
 								<div className="item_row" key={key}>
-									{/* <img className="item_thumb" alt = "" src={myObject[key]['dist_coupon_image_url']}></img> */}
+
+
+									{/* <ReactTransitionGroup.CSSTransition
+										key={person.id}
+										classNames={{
+											enter: "animated",
+											enterActive: "zoomIn",
+											exit: "animated",
+											exitActive: "zoomOut"
+										}} */}
+
 									<img className="item_thumb" alt="" src={myObject[key]['x_large_image_url']}></img>
-									{/* <span className="item_name">{myObject[key]['name']}</span> */}
+
+{/* </ReactTransitionGroup.CSSTransition> */}
+
+
+
 									<div>
 										<span className="item_name">
 											{myObject[key]['name']}
@@ -140,9 +180,14 @@ function OutSpec(props) {
 										${form_price(myObject[key]['current_price'])}{myObject[key]['price_text']}
 									</span>
 								</div>
+								
 							);
+							{/* </ReactTransitionGroup.TransitionGroup> */}
 						} else return null;
+					// } else return null;
 					} else return null;
+
+
 				});
 		}
 	}
@@ -181,27 +226,58 @@ function App(props) {
 			"https://circular.giantfood.com/flyers/giantfood?type=2&show_shopping_list_integration=1&postal_code=22204&use_requested_domain=true&store_code=0774&is_store_selection=true&auto_flyer=&sort_by=#!/flyers/giantfood-weekly?flyer_run_id=406535"
 
 		fetch(proxyurl + url_api) // https://cors-anywhere.herokuapp.com/https://example.com  Method to avoid/disable CORS errors in Chrome during development.
-			.then(response => response.text())
-			.then(htmlStr => {
-				var str = htmlStr;
-				var pos = str.search("current_flyer_id");
-				var res = str.slice(pos + 18, pos + 25);
-				return res;
+		.then(response => response.text())
+		.then(htmlStr => {
+			var str = htmlStr;
+			var pos = str.search("current_flyer_id");
+			var res = str.slice(pos + 18, pos + 25);
+			return res;
+		})
+		.then(id => {
+
+			url = "https://circular.giantfood.com/flyer_data/" + id + "?locale=en-US"
+
+			fetch(proxyurl + url)
+			.then(function (response) {
+				return response.json();
 			})
-			.then(id => {
+			.then(function(stuff) {
+				var stuffy;
+				stuffy = stuff.items;
+				
+				
+				
+				console.log("stuffy is");
+				// console.log(stuffy);
+				var testy;
 
-				url = "https://circular.giantfood.com/flyer_data/" + id + "?locale=en-US"
 
-				fetch(proxyurl + url)
-					.then(function (response) {
-						return response.json();
-					})
-					.then(function (myJson) {
-						console.log("API data loaded.");
-						setData(myJson);
-					})
-					.catch(() => console.log("Message from Carl's code:  can’t access " + url + " response. Blocked by browser?"));
-			});
+
+
+
+				var testy = Object.keys(stuffy).filter(key => {
+					if ((stuffy[key]["category_names"][0] === "Meat") ||
+						stuffy[key]["category_names"][0] === "Deli") {
+						return true;
+					}
+				});
+
+				testy = testy.map(function (key, index) {
+					return stuffy[key]
+				});
+
+				console.log(testy);
+
+				return stuffy;
+			})
+			.then(function (myJson) {
+				console.log("API data loaded.");
+				console.log(myJson);
+
+				setData(myJson);
+			})
+			.catch(() => console.log("Message from Carl's code:  can’t access " + url + " response. Blocked by browser?"));
+		});
 		/* End code to fetch API data. */
 
 	}, []);//Note the empty array passed as the second input parameter.  This ensures everything inside 'useEffect' is executed only once, on page load.  If this second parameter is omitted, everything in 'useEffect' will be executed every time state is updated.
@@ -209,6 +285,16 @@ function App(props) {
 	/* Function to ensure the 'meat' piece of state is updated every time the drop-down menu changes.*/
 	function handleInput(event) {
 		setMeat(event.target.value);
+
+		var i;
+		var rbs = document.getElementsByTagName("input") 
+		for (i = 0; i < rbs.length; i++) {
+			if (rbs[i].value == event.target.value) {
+				rbs[i].className = "radio animated heartBeat";	
+			} else {
+				rbs[i].className = "radio";	
+			}
+		}
 	}
 
 	/* Everything inside this return() statement is executed whenever state is updated. */
@@ -217,53 +303,45 @@ function App(props) {
 			<div className="filter">
 
 					
-				{/* <label htmlFor="meatChoice1">
-					Beef<br />
-					<input type="radio" id="meatChoice1" name="meaty" value="Beef" className="radio" onChange={handleInput} />
-				</label>
-			
-				<label htmlFor="meatChoice2">
-					Poultry<br />
-					<input type="radio" id="meatChoice2" name="meaty" value="Poultry" className="radio" onChange={handleInput} />
-				</label>
-
-				<label htmlFor="meatChoice3">
-					Pork<br />
-					<input type="radio" id="meatChoice3" name="meaty" value="Pork" className="radio" onChange={handleInput} />
-				</label> */}
-						
-
-
-
-
 
 				<div className="rad-lab" >
-					<label htmlFor="meatChoice1">
+					<label htmlFor="allmeat">
+						<img className="rad-lab-img" src={img_meat}>
+						</img>
+					</label>
+					<input type="radio" id="allmeat" name="meaty" value="" className="radio" onChange={handleInput} defaultChecked/>
+					<label className="rad-lab-txt" htmlFor="allmeat">
+						All Meat
+					</label>
+				</div>
+
+				<div className="rad-lab" >
+					<label htmlFor="Beef">
 						<img className="rad-lab-img" src={img_beef}>
 						</img>
 					</label>
-					<input type="radio" id="meatChoice1" name="meaty" value="Beef" className="radio" onChange={handleInput} />
-					<label className="rad-lab-txt" htmlFor="meatChoice1">
+					<input type="radio" id="Beef" name="meaty" value="Beef" className="radio" onChange={handleInput} />
+					<label className="rad-lab-txt" htmlFor="Beef">
 						Beef
 					</label>
 				</div>
 				<div className="rad-lab" >
-					<label htmlFor="meatChoice2">
+					<label htmlFor="Poultry">
 						<img className="rad-lab-img" src={img_chicken}>
 						</img>
 					</label>
-					<input type="radio" id="meatChoice2" name="meaty" value="Poultry" className="radio" onChange={handleInput} />
-					<label className="rad-lab-txt" htmlFor="meatChoice2">
-						Chicken
+					<input type="radio" id="Poultry" name="meaty" value="Poultry" className="radio" onChange={handleInput} />
+					<label className="rad-lab-txt" htmlFor="Poultry">
+						Poultry
 					</label>
 				</div>
 				<div className="rad-lab" >
-					<label htmlFor="meatChoice3">
+					<label htmlFor="Pork">
 						<img className="rad-lab-img" src={img_ribs}>
 						</img>
 					</label>
-					<input type="radio" id="meatChoice3" name="meaty" value="Pork" className="radio" onChange={handleInput} />
-					<label className="rad-lab-txt" htmlFor="meatChoice3">
+					<input type="radio" id="Pork" name="meaty" value="Pork" className="radio" onChange={handleInput} />
+					<label className="rad-lab-txt" htmlFor="Pork">
 						Pork
 					</label>
 				</div>
