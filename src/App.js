a580ac00323b1b4e068aa70fa5cd6914707baff7
Carl Gross <10165488+cagross@users.noreply.github.com>
2019-08-14 19:17:21 +0700

    Add unit pricing and header description.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@
 import React from 'react';
 import {CSSTransition} from 'react-transition-group'
 
-import './App.css';
-import './animate.css';
+import './App.css';// Import the main CSS file.
+import './animate.css';// Needed to fade transition to function.
 
 import { useState } from 'react';// This needs to be present in order to use the 'useState' hook.
 import { useEffect } from 'react';// This needs to be present in order to use the 'useEffect' hook.
@@ -29,11 +29,11 @@ function Results(props) {// Filter the list of specials based on the user's meat
 	
 	function meatList() {
 
-		var meatData = props.data;
-		var meatTerms;
+		const meatData = props.data;
+		let meatTerms;
 
 		//Definie all possible search terms here--terms which are indicative of a poultry, beef, or pork item.  This is very incomplete, as determining accurate terms is the difficult part.  This ia also  something that will eventually be moved to a database.
-		var searchTerms = {
+		const searchTerms = {
 			poultry: [
 				"chicken",
 				"roaster",
@@ -83,14 +83,14 @@ function Results(props) {// Filter the list of specials based on the user's meat
 			return Object.keys(meatData).map(function (key, index) {// Loop over every key in the weekly specials object and check if its name contains any of the meat search terms.  If so, render a row of information to the page.
 
 				/* Begin code to check if item name contains any meat terms. */
-				var pos;
-				var i;
-				var str = meatData[key]["display_name"].toLowerCase();
+				let pos;
+				// var i;
+				const str = meatData[key]["display_name"].toLowerCase();
 					
 				if (props.meat === "") {
 					pos = str.search(props.meat);
 				} else {
-					for (i = 0; i < meatTerms.length; i++) {
+					for (let i = 0; i < meatTerms.length; i++) {
 						pos = str.search(meatTerms[i]);
 						if (pos >= 0) {
 							break;
@@ -151,9 +151,9 @@ function Results(props) {// Filter the list of specials based on the user's meat
 								<span className="item_price">
 									${formPrice(meatData[key]['current_price'])}{meatData[key]['price_text']}
 								</span>
-								{/* <span className="item_price">
-									${form_price(meatData[key]['unit_price'])}/lb.
-								</span>								 */}
+								<span className="item_price">
+									${formPrice(meatData[key]['unit_price'])}/lb.
+								</span>
 							</div>
 						</CSSTransition>
 					);
@@ -177,41 +177,63 @@ function App(props) {
 	const [data, setData] = useState({});// Set a piece of state named 'data' to an empty object.  To update that piece of state, run the 'setData()' function.
 	const [meat, setMeat] = useState('');// Set a piece of state named 'meat' to an empty string.  To update that piece of state, run the 'setMeat()' function.
 
-	/* Execute the 'useEffect' hook to fetch the API data.  Pass a second parameter to useEffect()--a blank array--to ensure this is executed  only once (on initial page load ). */
+	/* Execute the 'useEffect' hook to fetch the API data.  Pass a second parameter to useEffect()--a blank array--to ensure this is executed only once (on initial page load ). */
 	useEffect(() => {
 
 		/* Begin code to fetch all weekly special data from the Giant Food API. */
 		const proxyurl = "https://cors-anywhere.herokuapp.com/";
 		const url_api1 =
 			"https://circular.giantfood.com/flyers/giantfood?type=2&show_shopping_list_integration=1&postal_code=22204&use_requested_domain=true&store_code=0774&is_store_selection=true&auto_flyer=&sort_by=#!/flyers/giantfood-weekly?flyer_run_id=406535"
-		var url_api2;
 
 		fetch(proxyurl + url_api1) // https://cors-anywhere.herokuapp.com/https://example.com  Method to avoid/disable CORS errors in Chrome during local development.
 		//Use this first fetch() to obtain just the flyer ID, which we will in-turn use with a second fetch() to obtain the actual weekly specials data.
 		.then(response => response.text())
 		.then(infoAPI2 => {
-			var pos = infoAPI2.search("current_flyer_id");
-			var id_api2 = infoAPI2.slice(pos + 18, pos + 25);
+			const pos = infoAPI2.search("current_flyer_id");
+			const id_api2 = infoAPI2.slice(pos + 18, pos + 25);
 			return id_api2;// This is the flyer ID that we will use with a second fetch() to obtain actual weekly specials data.
 		})		
 		.then(id => {
-			url_api2 = "https://circular.giantfood.com/flyer_data/" + id + "?locale=en-US"
-			fetch(proxyurl + url_api2)
-			//This fetch() obtains an object containing all weekly specials data from the Giant Food store in-question.
+			const url_api2 = "https://circular.giantfood.com/flyer_data/" + id + "?locale=en-US"
+			fetch(proxyurl + url_api2)//This fetch() obtains an object containing all weekly specials data from the Giant Food store in-question.
 			.then(response => response.json())
 			.then(dataAll => {
-
 				//Filter the weekly specials to return only the specials on meat.
-				var dataItems = dataAll.items;
-				var dataMeatItems = Object.keys(dataItems).filter(key => {
+				const dataItems = dataAll.items;
+				let dataMeatItems = Object.keys(dataItems).filter(key => {
 					if ((dataItems[key]["category_names"][0] === "Meat") ||
 						dataItems[key]["category_names"][0] === "Deli") {
 						return true;
 					}
 				});
 				dataMeatItems = dataMeatItems.map(function (key, index) {
-					var part = dataItems[key];
-					// part['unit_price'] = 5.55;// Set the unit price of each item here (incomplete as of 15/7/19)
+					
+					let part = dataItems[key];
+					part['unit_price'] = 5.55;
+
+					const pos_lb = part['price_text'].search("lb");// Search the 'price text' of each item for 'lb.'
+
+					if (pos_lb >= 0) {// If 'lb' occurs in the 'price text' of an item, then its 'current price' is already its unit price, so set it accordingly.
+						part['unit_price'] = part['current_price'];
+					} else {// If 'lb' does not occur in the 'price text' of an item, continue to determine the unit price using other methods.
+
+						const patt_ea = /\/ea/;
+						const has_ea = patt_ea.test(part['price_text']);// Check if the string 'ea' exists in the 'price text.'
+						// If 'ea' occurs in the 'price text,' or the 'price text' is blank, then assume the price is per package, and run the following code which searches through the item 'description' to determine the weight of the package.
+						if (has_ea || part['price_text'] === "") {
+
+							if (part['description'] != null) {
+								const pos_oz = part['description'].search(/oz\./i);// Search for the string 'oz' in the item 'description.'  Return the index in the string.
+
+								if (pos_oz >= 0) {// If the string 'oz' appears in the item 'description,' run the following code to extract the weight of the item.
+									const partial_oz = part['description'].substring(0, pos_oz);
+									const weight_oz = partial_oz.match(/[0-9]+/);
+									part['unit_price'] = 16*part['current_price']/weight_oz;// Calculate the per pound unit price of the item, using the total price and weight in ounces.
+								}
+							}
+						}
+					}
+
 					return part;
 				});
 				setData(dataMeatItems);
@@ -227,9 +249,8 @@ function App(props) {
 		setMeat(event.target.value);
 
 		// Add necessary CSS classes to radio button elements, ensuring their animations function as expected.
-		var i;
-		var radButtons = document.getElementsByTagName("input") 
-		for (i = 0; i < radButtons.length; i++) {
+		const radButtons = document.getElementsByTagName("input") 
+		for (let i = 0; i < radButtons.length; i++) {
 			if (radButtons[i].value === event.target.value) {
 				radButtons[i].className = "radio animated heartBeat";	
 			} else {
