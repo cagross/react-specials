c789c5d9d04bd1bbc05cecd1c03cfdb4c23d4a0c
Carl Gross <10165488+cagross@users.noreply.github.com>
2020-04-02 18:10:22 +0700

    Move API fetch to module, and ensure email service can use it.

--- module.js
+++ module.js
@@ -16,6 +16,50 @@ export function hello() {
 
 			return fetch(proxyURL + urlAPIFlyer) // e.g. https://cors-anywhere.herokuapp.com/https://example.com  Method to avoid/disable CORS errors in Chrome during local development.
 			.then(response => response.text())
+			// .then(response => response.json())
+			.then(flyerInfo => {
+				const posFlyerID = flyerInfo.search("current_flyer_id");
+				const flyerID = flyerInfo.slice(posFlyerID + 18, posFlyerID + 25);
+				const urlAPIData = "https://circular.giantfood.com/flyer_data/" + flyerID + "?locale=en-US";
+				fetch(proxyURL + urlAPIData)// This fetch() obtains an object containing all weekly specials data from the Giant Food store in-question.
+				return fetch(proxyURL + urlAPIData)// This fetch() obtains an object containing all weekly specials data from the Giant Food store in-question.
+
+				.then(response => response.json())
+				.then(dataAll => {
+					// console.log(dataAll);
+					// return 555;
+					const dataItems = dataAll.items;// Filter all data into only data related to items.
+					var dataMeatItems;
+
+					const filter = 1;// Set this to 1 to filter data into only meat/deli items.  Set this to any other value to apply no filtering (i.e. display all items on page).
+					const dataMeatItemsKeys = productFilter(dataItems, filter);// This returns an array of the keys after the desired filter has been applied.
+					
+					// dataMeatItems = dataMeatItemsKeys.map(function (key) {// Create a new array containing only filtered items.  In addition, calculate and add a unit price property to the array.
+					return dataMeatItems = dataMeatItemsKeys.map(function (key) {// Create a new array containing only filtered items.  In addition, calculate and add a unit price property to the array.
+			
+						let item = dataItems[key];
+
+						if (item['current_price'] === null) {//If an item has no price, set its price and unit price as unknown.
+							item['unit_price'] = 'unknown';
+							item['current_price'] = item['unit_price'];
+						} else {
+							unitPrice(item);//Calculate the unit price of the item and add it to the items array.
+						}
+
+						return item;
+					});
+					// return 555;
+		// 			// console.log('fetched');
+		// 			// test();
+		// 			// return dataMeatItems;
+		// 			// return "Hello from module.";
+
+		// 	// 		setData(dataMeatItems);//Assign this array (the array containing all desired items and information) to the value of the 'data' variable.
+				})
+				// return dataAll;
+
+				// return flyerInfo;
+			});
 
 
 
