7ec2e491045a9f5968213c7471f40f61acdf47ad
Carl Gross <10165488+cagross@users.noreply.github.com>
2020-04-04 01:03:17 +0700

    Troubleshoot filter module issues.

--- src/module-filter.js
+++ src/module-filter.js
@@ -15,65 +15,33 @@ import { terms } from './module-terms.js';
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 // export function filter(meat) {
 export function filter(props) {
 	// console.log(props);
 	
+
+	let test123;
+	let testy;
+
+	// const test123;
+
+
 	const meatData = props.data;
-		const searchTerms = terms();
-		let meatPref;
-		// Determine which terms should be used to filter products, baed on the user's selected meat.
-		if (props.currMeat === "poultry") {
-			meatPref = searchTerms['poultry'];
-		} else if (props.currMeat === "beef") {
-			meatPref = searchTerms['beef'];
-		} else if(props.currMeat === "pork") {
-			meatPref = searchTerms['pork'];
-		}
-		if (Object.entries(meatData).length) {// Check if the weekly specials array is empty or not.  If it is not empty, execute code.
-			const filterObject = (obj) => 
-				Object.keys(obj).reduce(function (acc, val) {
-					const itemName = obj[val]["display_name"].toLowerCase();
-					console.log('name is: ' + itemName);
-					let match = false;
-					if (props.currMeat === "") {
-						match = true;
-					} else {
-						for (let i = 0; i < meatPref.length; i++) {
-							const pos = itemName.search(meatPref[i]);
-							if (pos >= 0) {
-								match = true;
-								break;
-							}
-						}
-					}
-					if (match) {
-						return {
-							...acc,[val]: obj[val]
-						};
-					} else {
-						return acc;
-					}
-				}
-		  	);
-			const test123 = filterObject(meatData);
-			console.log(test123);
-		
-			Object.keys(meatData).map(function (key) {// Loop over every key in the weekly specials array and check if it contains any of the meat search terms.  If so, render a row of information to the page.
-		 		/* Begin code to check if item name contains any search terms. */
-				const itemName = meatData[key]["display_name"].toLowerCase();
+	const searchTerms = terms();
+	let meatPref;
+	// Determine which terms should be used to filter products, baed on the user's selected meat.
+	if (props.currMeat === "poultry") {
+		meatPref = searchTerms['poultry'];
+	} else if (props.currMeat === "beef") {
+		meatPref = searchTerms['beef'];
+	} else if(props.currMeat === "pork") {
+		meatPref = searchTerms['pork'];
+	}
+	if (Object.entries(meatData).length) {// Check if the weekly specials array is empty or not.  If it is not empty, execute code.
+		const filterObject = (obj) => 
+			Object.keys(obj).reduce(function (acc, val) {
+				const itemName = obj[val]["display_name"].toLowerCase();
+				// console.log('name is: ' + itemName);
 				let match = false;
 				if (props.currMeat === "") {
 					match = true;
@@ -82,13 +50,42 @@ export function filter(props) {
 						const pos = itemName.search(meatPref[i]);
 						if (pos >= 0) {
 							match = true;
-							console.log('type: ' + typeof meatData);
 							break;
 						}
 					}
 				}
-				/* End code to check if item name contains any meat terms. */
-			});
-		}
-		return meatData;
+				if (match) {
+					return {
+						...acc,[val]: obj[val]
+					};
+				} else {
+					return acc;
+				}
+			}
+		);
+
+		// const test123 = filterObject(meatData);
+		test123 = filterObject(meatData);
+		// console.log('inside:');
+		// console.log(test123);
+		// console.log(Object.keys(test123).length);
+		
+		
+		// return test123;
+		// testy = 555;
+	} else {
+		// testy = 123;
+		test123 = meatData;
+	}
+	console.log('outside:');
+	// console.log(test123);
+	// console.log(testy);
+
+	// console.log(Object.keys(test123).length);
+	console.log(Object.keys(meatData).length);
+
+	// return test123;
+	return meatData;
+	
+
 }
