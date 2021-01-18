f7467025f16e79508fd85bcd8e8a343338ea2c66
Carl Gross <cagross@everlooksolutions.com>
2021-01-18 20:18:49 +0700

    Test fix to CORS issue introduced in previous commit.

--- src/module.js
+++ src/module.js
@@ -8,17 +8,22 @@ export function apiData() {
 		// If this module is being called by the front-end app, it will need to have a valid proxy URL set.  Without it, fetching data will fail (at least it will in Chrome).  See the project's README file for more information on setting up a valid proxy URL.
     let proxyURL = ''
     console.log(555);
-    console.log(process.env.CORS_PROXYURL);
-
-    if (process.env.CORS_PROXYURL) {// If an environmental variable named CORS_PROXYURL exists, set its value as the proxy URL.
+    console.log(window.location.href);
+    if (window.location.href) {// If an environmental variable named CORS_PROXYURL exists, 
       console.log(666);
-      console.log(process.env.CORS_PROXYURL);
-      proxyURL = process.env.CORS_PROXYURL
-      console.log(proxyURL);
+      console.log(window.location.href);
+      if (window.location.href === 'http://localhost:3000/') {// If an environmental variable named CORS_PROXYURL exists, set its value as the proxy URL.
+        proxyURL = ''
+        console.log(777);
+        console.log(proxyURL);
+        console.log("Ensure you have a valid CORS proxy solution. See project's README file.");
+      } else {
+        console.log(888);
+        proxyURL = 'https://sheltered-lake-52088.herokuapp.com/';// This proxy URL is valid only for the Specials app deployed to https://gentle-gorge-04163.herokuapp.com/.  If your app is deployed locally
+      }
     }
-    console.log(777);
+    console.log(999);
     console.log(proxyURL);
-    // proxyURL = 'https://sheltered-lake-52088.herokuapp.com/';// This proxy URL is valid only for the Specials app deployed to https://gentle-gorge-04163.herokuapp.com/.  If your app is deployed locally
     		
 // Check if the back-end app is in use by checking for the window object.  If it does not exist, then the module is being called by the back-end app, and the proxy URL does not need to be set.
 		if (typeof window === 'undefined') {
