6e00fc12ed322d6940bde84bdb30e7656c957b63
Carl Gross <cagross@everlooksolutions.com>
2021-01-31 22:08:44 +0700

    Cleaned up code.

--- src/module-store-location.js
+++ src/module-store-location.js
@@ -1,22 +1,5 @@
-// export function loc() {
-// 	const storeLoc = [
-// 			'Giant Food',
-// 			'7235 Arlington Blvd',
-// 			'Falls Church, VA 22042'
-// 	];
-// 	return storeLoc;
-// }
-
 export const storeLoc = [
 	'Giant Food',
 	'7235 Arlington Blvd',
 	'Falls Church, VA 22042'
 ];
-
-// export const storeLoc = 'Giant Food';
-
-// export const storeLoc = [
-// 	123,
-// 	555,
-// 	666
-// ];
