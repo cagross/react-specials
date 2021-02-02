1d54e74b564f108619f52711f07e8805fa8462c8
Carl Gross <cagross@everlooksolutions.com>
2021-02-02 23:08:06 +0700

    Add/Clean up comments.

--- src/module-terms.js
+++ src/module-terms.js
@@ -1,10 +1,7 @@
 //Define all possible search terms here--terms which are indicative of a poultry, beef, or pork item.  These terms are requried to determine which meat category each items belongs.  This is very incomplete and inexact, as determining accurate and complete search terms is difficult.  Also, this data should eventually be moved to a database.
 
 export function terms() {
-
-	// const searchTerms = {
 	const searchTerms = {
-
 		poultry: [
 			"chicken",
 			"roaster",
