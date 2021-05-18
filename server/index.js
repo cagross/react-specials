607196a69589970ff5a608427d87de8c50b1c6cb
Carl Gross <cagross@everlooksolutions.com>
2021-05-18 20:37:59 +0700

    Remove extraneous files.

--- server/index.js
+++ server/index.js
@@ -2,17 +2,32 @@ const path = require("path");
 const express = require("express");
 const app = express(); // create express app
 
-// add middlewares
-// app.use(express.static(path.join(__dirname, "..", "build")));
+// add middlewaree
+/* Begin middleware to ensure React app is served at localhost:5555 and all subdirectories. */
 app.use(express.static(path.join(__dirname, "..", "client", "build")));
-
 app.use(express.static("public"));
+/* Ensure a route to any path serves my React app.*/
+// app.use((req, res, next) => {
+//   res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+// });
+/* End middleware to ensure React app is served at localhost:5555 and all subdirectories. */
 
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+console.log(555);
+console.log(path.join(__dirname, "..", "client", "public"));
+
+// Create the /register route.
+app.use(express.json());
+// app.post("/register", (req, res) => {
+app.post("/register", (req, res) => {
+  const { username, password } = req.body;
+  // res.json("register response");
 });
-
-// app.use(express.static("public"));
+// app.post("/login", (req, res) => {
+//   res.json("login");
+// });
+// app.post("/profile", (req, res) => {
+//   res.json("profile");
+// });
 
 // app.get("/", (req, res) => {
 //   res.sendFile(path.join(__dirname, "public", "index.html"));
