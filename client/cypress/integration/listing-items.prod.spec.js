25c03598a5a4f6c85849594aaf8b0f3fc010ae8b
Carl Gross <cagross@everlooksolutions.com>
2022-01-19 20:37:02 -0500

    Remove end-to-end test of deployed app.

--- client/cypress/integration/listing-items.prod.spec.js
+++
@@ -1,9 +0,0 @@
-describe("Production URL: Listing Items", () => {
-  it("Displays items from the circular.", () => {
-    cy.visit("https://gentle-gorge-04163.herokuapp.com/");
-
-    //Ensure $items_container element is not empty.
-    cy.get("#items_container");
-    cy.contains(/.+/);
-  });
-});
--- client/cypress/integration/listing-items.spec.js
+++ client/cypress/integration/listing-items.spec.js
@@ -2,7 +2,7 @@ describe("Development URL: Listing Items", () => {
   it("Displays items from the circular.", () => {
     cy.visit("/");
 
-    //Ensure $items_container element is not empty.
+    //Ensure #items_container element is not empty.
     cy.get("#items_container");
     cy.contains(/.+/);
   });
