2e786bb0074a52c2cb339923e863a1130538c8a8
Carl Gross <cagross@everlooksolutions.com>
2025-03-29 11:42:17 +0700

    Fix issue with missing source maps during debugging.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build' // Ensures the build output is in the 'build' folder as CRA does.
-  }
+    outDir: "build", // Ensures the build output is in the 'build' folder as CRA does.
+    sourcemap: true,
+  },
 });
